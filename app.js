55c7b6926accf726d57aab893b40130cd1ababf1
Jaehwan Lim <jh968574@gmail.com>
2024-04-13 11:32:44 +0900

    add port .env

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
 const express = require('express');
 const route = require('./routes');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(express.json());
 
